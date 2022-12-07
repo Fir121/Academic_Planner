b011fbd934fa85cbe09c3ee8f0bdf4abdf3fb494
Fir121 <mohamedfiras2003@gmail.com>
2022-12-08 00:54:49 +0400

    Added courses screen

--- academic-planner/src/main/java/com/oops/AppFrame.java
+++ academic-planner/src/main/java/com/oops/AppFrame.java
@@ -3,6 +3,7 @@ package com.oops;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class AppFrame{
     static JFrame mainFrame;
@@ -132,7 +133,7 @@ class LoginFormPanel extends AppFrame implements ActionListener{
 
     public void actionPerformed(ActionEvent e){
         System.out.println(passwordField.getPassword());
-        // TODO FN CALL AND HANDLE HERE
+        new HomePanel();
     }
 }
 class RegisterFormPanel extends AppFrame implements ActionListener{
@@ -217,6 +218,7 @@ class RegisterFormPanel extends AppFrame implements ActionListener{
         System.out.println(emailField.getText());
         System.out.println(passwordField.getPassword());
         // TODO FN CALL AND HANDLE HERE
+		new HomePanel();
     }
 }
 
@@ -226,16 +228,123 @@ class HomePanel extends AppFrame{
         homePanel = new JPanel();
         mainFrame.setContentPane(homePanel);
 
-        JButton button = new JButton("Opt 1");
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{59, 0};
+		gbl_contentPane.rowHeights = new int[]{48, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		homePanel.setLayout(gbl_contentPane);
+		
+		Box verticalBox = Box.createVerticalBox();
+		GridBagConstraints gbc_verticalBox = new GridBagConstraints();
+		gbc_verticalBox.insets = new Insets(20, 20, 20, 20);
+		gbc_verticalBox.gridx = 0;
+		gbc_verticalBox.gridy = 0;
+		homePanel.add(verticalBox, gbc_verticalBox);
+
+		JPanel panel = new JPanel();
+		verticalBox.add(panel);
+        JButton button = new JButton("Courses");
 		button.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        homePanel.add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new CoursePanel();
+			}
+		});
+        panel.add(button);
+
+		JPanel panel1 = new JPanel();
+		verticalBox.add(panel1);
         JButton button1 = new JButton("Opt 2");
 		button1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        homePanel.add(button1);
+        panel1.add(button1);
+
+		JPanel panel2 = new JPanel();
+		verticalBox.add(panel2);
         JButton button2 = new JButton("Opt 3");
 		button2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        homePanel.add(button2);
+        panel2.add(button2);
 
         refresh();
     }
+}
+
+class CoursePanel extends AppFrame{
+	JPanel coursePanel;
+	JButton addCourse;
+
+	ArrayList<String> courses;
+	public CoursePanel(){
+		coursePanel = new JPanel();
+        mainFrame.setContentPane(coursePanel);
+
+		courses = Courses.getCourses();
+
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{59, 0};
+		gbl_contentPane.rowHeights = new int[]{48, 107, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		coursePanel.setLayout(gbl_contentPane);
+
+		Box verticalBox = Box.createVerticalBox();
+		GridBagConstraints gbc_verticalBox = new GridBagConstraints();
+		gbc_verticalBox.gridx = 0;
+		gbc_verticalBox.gridy = 0;
+		coursePanel.add(verticalBox, gbc_verticalBox);
+
+		for (String course:courses){
+			JPanel panel = new JPanel();
+			panel.setBorder(null);
+			verticalBox.add(panel);
+			
+			JLabel lblNewLabel = new JLabel(course);
+			panel.add(lblNewLabel);
+
+			JButton btnNewButton_2 = new JButton("Remove");
+			btnNewButton_2.putClientProperty( "course", course);
+			btnNewButton_2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if (Courses.removeCourse((String)((JButton)e.getSource()).getClientProperty( "course" ))){
+						new CoursePanel();
+					}
+					else{
+						//failed
+					}
+				}
+			});
+			panel.add(btnNewButton_2);
+		}
+
+		addCourse = new JButton("Add Course");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 1;
+		addCourse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextField courseName = new JTextField();
+				JTextField courseId = new JTextField();
+				JSpinner courseCredits = new JSpinner();
+				Object[] message = {
+					"Course Name:", courseName,
+					"Course ID:", courseId,
+					"Course Credits:",courseCredits
+				};
+
+				int option = JOptionPane.showConfirmDialog(null, message, "Login", JOptionPane.OK_CANCEL_OPTION);
+				if (option == JOptionPane.OK_OPTION) {
+					if (Courses.addCourse(courseName.getText(), courseId.getText(), (Integer)courseCredits.getValue())){
+						new CoursePanel();
+					}
+					else{
+						// failed
+					}
+				} else {
+					// none
+				}
+			}
+		});
+		coursePanel.add(addCourse, gbc_btnNewButton);
+		refresh();
+	}
 }
---
+++ academic-planner/src/main/java/com/oops/Courses.java
@@ -0,0 +1,18 @@
+package com.oops;
+
+import java.util.*;
+
+public class Courses {
+    public static ArrayList<String> getCourses(){
+        ArrayList<String> al = new ArrayList<>();
+        al.add("CS F111 Comp Prog");
+        al.add("CHEM F111 Chem");
+        return al;
+    }
+    public static boolean removeCourse(String course){
+        return true;
+    }
+    public static boolean addCourse(String courseName, String courseId, Integer courseCredits){
+        return true;
+    }
+}
